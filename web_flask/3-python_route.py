3f0c5e76cc92759b61893012901903ca5a10ec13
ZAKARIA TOURAS <zakariatouras47@gmail.com>
2024-07-31 18:41:50 -0700

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ display Hello HBNB! message """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ displays HBNB message for hbnb page """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_text(text):
+    """ displays C followed by text for C page """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """ displays python followed by text """
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
